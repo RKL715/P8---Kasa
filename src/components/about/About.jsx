1ccfd8b1df7f16b9d84dbc658dd4675d411c40c7
RKL715 <remykennyl715@gmail.com>
2024-02-20 10:32:28 +0100

    Added Routes, Nav between About/Home with React Router

---
+++ src/components/about/About.jsx
@@ -0,0 +1,17 @@
+import Header from "../common/Header.jsx";
+import Footer from "../common/Footer.jsx";
+
+function About() {
+    return (
+        <div>
+            <Header />
+            <h1>About</h1>
+            <p>
+                Kasa is a company that offers a wide range of properties for rent.
+            </p>
+            <Footer />
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../common/Header.jsx";
+import Footer from "../common/Footer.jsx";
+import Banner from "../common/Banner.jsx";
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Banner />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
