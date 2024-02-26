9f562a1f9ba7035c9a654482d81cf8d743e7886d
RKL715 <remykennyl715@gmail.com>
2024-02-26 16:32:23 +0100

    dynamic card generation /replace card placeholder

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -1,19 +1,23 @@
 import Card from "./Card.jsx";
+import logementData from '/public/logements.json' // import the logementData from the logements.json file
 
-function Gallery (titreLocation) {
+const Gallery = () => {
     return (
-        <div className={'gallery-background'}>
-            <div className={'gallery'}>
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-                <Card title={'Lorem Ipsum'} alt="random" image="https://picsum.photos/200/300" />
-            </div>
+        <div className={"gallery-background"}>
+            <div className={"gallery"}>
+            {logementData.map((logement) => (  // map through the logementData array and render a Card component for each logement
+                <Card
+                    key={logement.id}  // pass the id, figcaption, alt, and image props to the Card component
+                    id={logement.id}
+                    figcaption={logement.title}
+                    alt={logement.title}
+                    image={logement.cover}/>
+                )
+            )}
         </div>
-    )
-}
+        </div>
+            )
+            }
 
 
 
