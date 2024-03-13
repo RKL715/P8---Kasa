e8ea40c6cfe11608c35a01314aa80716de6d84ac
RKL715 <remykennyl715@gmail.com>
2024-03-13 13:32:12 +0100

    Deleted Header and footer from every pages and added them directly on APP.JSX

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,13 @@
-import Header from "../common/Header.jsx";
-import Footer from "../common/Footer.jsx";
 import Banner from "../common/Banner.jsx";
 import Gallery from "./Gallery.jsx";
 
 function Home() {
     return (
         <div>
-            <Header />
+
             <Banner imgSrc='/banner.png' text='Chez vous, partout et ailleurs'/>
             <Gallery />
-            <Footer />
+
         </div>
     )
 }
